5d69482e3f9507b7634c19cc52ee69930aa6cad2
CEDipEngineering <carlosed3@al.insper.com.br>
2020-02-27 14:25:33 -0300

    Cleaned up atividade2.py, and restarted BRISK. Problems with cv2.imread().

--- Atividade_02/atividade2.py
+++ Atividade_02/atividade2.py
@@ -2,7 +2,7 @@
 
 # -*- coding: utf-8 -*-
 
-# __author__      = "Matheus Dib, Fabio de Miranda" Modificado
+# __author__      = "Matheus Dib, Fabio de Miranda" ==> Modificado
 __author__ = "Carlos Dip"
 
 
@@ -12,15 +12,10 @@ from matplotlib import pyplot as plt
 import time
 import auxiliar as aux
 
-# If you want to open a video, just change this path
-#cap = cv2.VideoCapture('hall_box_battery.mp4')
 
-# Parameters to use when opening the webcam.
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-img_logo = cv2.imread("insper.jpg")
-img_logo = cv2.cvtColor(img_logo, cv2.COLOR_BGR2RGB)
 
 lower = 0
 upper = 1
@@ -29,15 +24,10 @@ magenta = "#F08020"
 blue = "#586B7D"
 m1,m2 = np.array([172, 50, 50]), np.array([180, 255, 255])
 b1,b2 = np.array([80, 50, 50]), np.array([110, 255, 255])
-
-# BRISK
-brisk = cv2.BRISK_create()
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontsize = 2
 
 # BRISK no logo INSPER
-kplogo, deslogo = brisk.detectAndCompute(img_logo, None) 
-MIN_MATCH_COUNT = 10
             
 
 # Returns an image containing the borders of the image
@@ -90,45 +80,25 @@ while(True):
     # Captura da imagem
     ret, frame = cap.read()
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+    framed = frame.copy()
+    frame_out = frame.copy()
     # Tratamento da imagem (Detecção de bordas)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray,(5,5),0)
     bordas = auto_canny(blur)
     bordas_color = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
 
-    # BRISK
 
-    kpframe, desframe = brisk.detectAndCompute(bordas_color,None)
-    
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-
-    matches = bf.knnMatch(deslogo,desframe,k=2)
-
-    good = []
-    for m,n in matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
-
-
-    if len(good)>MIN_MATCH_COUNT:
-        # Separa os bons matches na origem e no destino
-        print("Matches found")    
-        framed = find_homography_draw_box(kplogo, kpframe, bordas_color)
-    else:
-        print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
-    # Seleção das imagens por bordas
-
-    # frame_mag = cv2.inRange(frame_hsv,m1,m2)
-    # frame_blu = cv2.inRange(frame_hsv,b1,b2)
-    # frame_mag = cv2.morphologyEx(frame_mag,cv2.MORPH_CLOSE,np.ones((4, 4)))
-    # frame_blu = cv2.morphologyEx(frame_blu,cv2.MORPH_CLOSE,np.ones((4, 4)))
-    # contornos_mag, arvore = cv2.findContours(frame_mag.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-    # contornos_blu, arvore = cv2.findContours(frame_blu.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # frame_mag_out = frame.copy()
-    # frame_blu_out = frame.copy()
-    # cv2.drawContours(frame_mag_out, contornos_mag, -1, [0, 0, 255], 3)
-    # cv2.drawContours(frame_blu_out, contornos_blu, -1, [0, 0, 255], 3) 
+    frame_mag = cv2.inRange(frame_hsv,m1,m2)
+    frame_blu = cv2.inRange(frame_hsv,b1,b2)
+    frame_mag = cv2.morphologyEx(frame_mag,cv2.MORPH_CLOSE,np.ones((4, 4)))
+    frame_blu = cv2.morphologyEx(frame_blu,cv2.MORPH_CLOSE,np.ones((4, 4)))
+    contornos_mag, arvore = cv2.findContours(frame_mag.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+    contornos_blu, arvore = cv2.findContours(frame_blu.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    frame_mag_out = frame.copy()
+    frame_blu_out = frame.copy()
+    cv2.drawContours(frame_mag_out, contornos_mag, -1, [0, 0, 255], 3)
+    cv2.drawContours(frame_blu_out, contornos_blu, -1, [0, 0, 255], 3) 
 
 
     # Deteccção de círculos (Hough Circles)
@@ -148,25 +118,25 @@ while(True):
             
             # Guarda o centro do círculo atual, e desenha seu centro e contorno
             circ_list.append((i[0],i[1]))
-            cv2.circle(bordas_color,(i[0],i[1]),i[2]-3,(int(r),int(g),int(b)),6)
-            cv2.circle(bordas_color,(i[0],i[1]),2,(int(r),int(g),int(b)),i[2])
+            cv2.circle(frame_out,(i[0],i[1]),i[2]-3,(int(r),int(g),int(b)),6)
+            cv2.circle(frame_out,(i[0],i[1]),2,(int(r),int(g),int(b)),i[2])
 
 
         # Desenha a reta e calcula a distância entre os círculos
         if len(circ_list) >= 2:
-            cv2.line(bordas_color, circ_list[0], circ_list[1], (255,255,255), 2) # Linha entre círculos
+            cv2.line(frame_out, circ_list[0], circ_list[1], (255,255,255), 2) # Linha entre círculos
             dx = np.abs(int(circ_list[0][0]) - int(circ_list[1][0]))
             dy = np.abs(int(circ_list[0][1]) - int(circ_list[1][1]))
             dist = float(np.sqrt(dx**2 + dy**2)) # Distância euclideana em pixels (h)
             real_dist = focus*14/dist # Distância real (D = F*H(14cm)/ h)
-            cv2.putText(bordas_color, str(real_dist), (5,fontsize*30), font, fontsize, (255,255,255))
+            cv2.putText(frame_out, "Distance: %.2f cm" %real_dist, (5,fontsize*30), font, fontsize, (255,255,255))
             # print(real_dist)
             angle = np.arctan(dy/dx)*180/np.pi
-            cv2.putText(bordas_color, str(angle), (5,fontsize*60), font, fontsize, (255,255,255))
+            cv2.putText(frame_out, "Angle: %.4f" % angle, (5,fontsize*60), font, fontsize, (255,255,255))
             # print(angle)
 
     # Display the resulting frame
-    cv2.imshow('Detector de circulos',bordas_color)
+    cv2.imshow('Detector de circulos',frame_out)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 # When everything done, release the capture
